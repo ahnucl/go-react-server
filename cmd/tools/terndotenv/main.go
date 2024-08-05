9e70eaebd1b3dd2226933be58cc114b6c19ef980
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-08-05 20:32:25 +0000

    Cria wrapper para o tern com env

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+
+	// if err := cmd.Run(); err != nil {
+	// 	panic(err)
+	// }
+
+	// Buffers para capturar a saída padrão e a saída de erro
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	// Executar o comando
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Erro ao executar o comando:", err)
+		fmt.Println("Saída de erro:", stderr.String())
+		return
+	}
+
+	// Imprimir a saída do comando se não houver erro
+	fmt.Println("Saída do comando:", out.String())
+}
